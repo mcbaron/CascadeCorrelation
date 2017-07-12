2dd9333cf20934402be61091312bb8ee82591764
Corvu <Corvu@users.noreply.github.com>
2017-07-12 17:49:05 +0300

    Optimized with type definition

--- ShufflePatterns.jl
+++ ShufflePatterns.jl
@@ -3,15 +3,14 @@
 function shuffle_patterns(training_set_in, training_set_out)
 
   n_patterns = size(training_set_in, 1)
-  temp_arr = zeros(n_patterns)
-  temp_bool = zeros(n_patterns)
+  temp_arr = Array{Int64}(n_patterns)
+  temp_bool = Array{Bool}(n_patterns)
   ret_in = zeros(size(training_set_in))
   ret_out = zeros(size(training_set_out))
   for i=1:n_patterns
     temp_arr[i] = i
   end
   temp_arr = shuffle(temp_arr)
-  temp_arr = convert(Array{Int,1}, temp_arr)
   for i=1:n_patterns
     ret_in[i,:] = training_set_in[temp_arr[i],:]
     ret_out[i] = training_set_out[temp_arr[i]]
